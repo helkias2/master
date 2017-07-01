dc70f7b5bd883e8763948b18daad32653e02d8ef
InovIF <toni.cpd@hotmail.com>
2017-07-01 01:24:53 -0300

    Update ProdutosDAO.js

--- casadocodigo/app/infra/ProdutosDAO.js
+++ casadocodigo/app/infra/ProdutosDAO.js
@@ -19,12 +19,4 @@ ProdutosDAO.prototype.lista = function(callback){
 };
 
 
-// module.exports = function(){
-// 	return function(connection){
-// 	    this.lista = function(callback){
-// 	      connection.query('select * from produtos', callback);
-// 	    };
 
-// 	    return this;
-// 	};
-// };
